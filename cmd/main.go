57a64e1f6108c2a8daace07c372090917243f016
SyedMTaseen <taseensy@gmail.com>
2022-08-08 12:57:02 +0000

    Result in cli fixed

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,8 @@ import (
 	"Mach/pkg/Request"
 	"Mach/pkg/Response"
 	"fmt"
+	"io/ioutil"
+	"strconv"
 )
 
 func main() {
@@ -12,32 +14,41 @@ func main() {
 
 	t := ReadFiles.ReadYml()
 
+	fmt.Println("Mach")
 	performMach(t)
 
 }
 
 func performMach(t map[interface{}]interface{}) {
 
-	fmt.Print(t["APITESTING"].(map[string]interface{})["Name"]) // Name
-	fmt.Print("\n")
-	RequestURL := t["APITESTING"].(map[string]interface{})["RequestURL"]
-	fmt.Println(RequestURL)
-	HTTPmethods := t["APITESTING"].(map[string]interface{})["HTTP-method"].(string)
-	fmt.Println(HTTPmethods)
-	testcases := t["APITESTING"].(map[string]interface{})["TestCases"].([]interface{})
+	fmt.Println("API Discription: " + t["Mach"].(map[string]interface{})["Name"].(string)) // Name
+	RequestURL := t["Mach"].(map[string]interface{})["RequestURL"]
+	fmt.Println("Request URL " + RequestURL.(string))
+	HTTPmethods := t["Mach"].(map[string]interface{})["HTTP-method"].(string)
+	fmt.Println("Method: " + HTTPmethods)
+	testcases := t["Mach"].(map[string]interface{})["TestCases"].([]interface{})
 	testcasesLen := len(testcases)
 	for i := 0; i < testcasesLen; i++ {
 		testcase := testcases[i]
 		Name := testcase.(map[string]interface{})["Name"]
-		fmt.Println(Name)
+		fmt.Print("Test case : " + Name.(string) + " ")
 
 		responce := Request.Request(testcase, RequestURL, HTTPmethods)
 
-		fmt.Println(responce.StatusCode)
+		//fmt.Println(responce.StatusCode)
 
 		result := Response.ResponceMain(testcase, responce)
 
-		fmt.Println(result)
+		if result.Result {
+			fmt.Println("PASSED")
+		} else {
+			fmt.Println("FAILED")
+			fmt.Println("Reason: " + result.Description)
+			resbody, _ := ioutil.ReadAll(responce.Body)
+			fmt.Println("Status: " + strconv.Itoa(responce.StatusCode))
+			fmt.Println("Response: " + string(resbody))
+		}
+		//fmt.Println(result)
 
 	}
 
