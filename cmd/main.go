32f6c5d9dcf14fdc0cabaf145432ce4bb4a10765
SyedMTaseen <taseensy@gmail.com>
2022-08-08 08:58:03 +0000

    result with discription added

--- cmd/main.go
+++ cmd/main.go
@@ -12,7 +12,7 @@ import (
 )
 
 // Test us the result of Testcases
-type TestCaseTests struct {
+type TestCaseResult struct {
 	Description string //Whats gone wrong
 	Result      bool   // Pass or fail
 }
@@ -83,14 +83,13 @@ func performMach(t map[interface{}]interface{}) {
 
 }
 
-func ResponceMain(testcase interface{}, responce *http.Response) bool {
+func ResponceMain(testcase interface{}, responce *http.Response) *TestCaseResult {
 
-	result := true
 	Responces := testcase.(map[string]interface{})["Responces"]
 	StatusCode := Responces.(map[string]interface{})["StatusCode"].(int)
-	result = checkStatusCode(StatusCode, responce.StatusCode)
-	if result == false {
-		return false
+	result := checkStatusCode(StatusCode, responce.StatusCode)
+	if !result {
+		return newTestCaseResult("Status Code Not Matched", false)
 	}
 	Body := Responces.(map[string]interface{})["Body"]
 	respObj := ResponcetoObject(responce)
@@ -108,7 +107,7 @@ func checkStatusCode(statusCode int, responceStatusCode int) bool {
 
 }
 
-func bodyContains(Contains interface{}, respObj interface{}) bool {
+func bodyContains(Contains interface{}, respObj interface{}) *TestCaseResult {
 
 	if Contains.(map[string]interface{})["Type"] == "List" {
 
@@ -116,7 +115,7 @@ func bodyContains(Contains interface{}, respObj interface{}) bool {
 		case []interface{}:
 			return list(Contains, respObj)
 		default:
-			return false
+			return newTestCaseResult("No List Found", false)
 		}
 
 	} else if Contains.(map[string]interface{})["Type"] == "Object" {
@@ -124,21 +123,21 @@ func bodyContains(Contains interface{}, respObj interface{}) bool {
 		case map[string]interface{}:
 			return object(Contains, respObj)
 		default:
-			return false
+			return newTestCaseResult("No Obj Found", false)
 		}
 
 	} else {
-		return false
+		return newTestCaseResult("Passed", true)
 	}
 
 }
 
-func list(Contains interface{}, respObj interface{}) bool {
+func list(Contains interface{}, respObj interface{}) *TestCaseResult {
 	Lenght := Contains.(map[string]interface{})["Lenght"]
 	if Lenght.(map[string]interface{})["Equal"] != nil {
 		leng := Lenght.(map[string]interface{})["Equal"]
-		if Equal(leng.(int), len(respObj.([]interface{}))) == false {
-			return false
+		if !Equal(leng.(int), len(respObj.([]interface{}))) {
+			return newTestCaseResult("List Lenght not equal", false)
 		}
 	}
 	InType := Contains.(map[string]interface{})["InType"]
@@ -146,42 +145,46 @@ func list(Contains interface{}, respObj interface{}) bool {
 		return listValue(InType, respObj)
 	}
 
-	return true
+	return newTestCaseResult("Passed", true)
 }
 
-func listValue(InType interface{}, respObj interface{}) bool {
+func listValue(InType interface{}, respObj interface{}) *TestCaseResult {
 	intype := InType.([]interface{})[0]
 	//fmt.Println(intype)
 
 	for key, value := range intype.(map[interface{}]interface{}) {
+		if len(intype.(map[interface{}]interface{})) < key.(int) {
+			return newTestCaseResult("List item not found", false)
+		}
 		obj := respObj.([]interface{})[key.(int)]
 		if obj == nil {
-			return false
+			return newTestCaseResult("List item not found", false)
 		}
 		switch value.(type) {
 		case map[string]interface{}:
 			fmt.Println(key, reflect.TypeOf(value))
-			if bodyContains(value.(map[string]interface{})["Contains"], obj) == false {
-				return false
+			res := bodyContains(value.(map[string]interface{})["Contains"], obj)
+			if !res.Result {
+				return res
 			}
 			//fmt.Println("Integer:", value.(map[string]interface{})["Contains"])
 		default:
 			fmt.Println(key, reflect.TypeOf(value))
-			if ListChecks(value, obj) == false {
-				return false
+			if !ListChecks(value, obj) {
+				return newTestCaseResult("List item not matched", false)
 			}
 		}
 
 	}
-	return true
+	return newTestCaseResult("Passed", true)
 }
 
-func object(Contains interface{}, respObj interface{}) bool {
+func object(Contains interface{}, respObj interface{}) *TestCaseResult {
 	Lenght := Contains.(map[string]interface{})["Lenght"]
 	if Lenght.(map[string]interface{})["Equal"] != nil {
 		leng := Lenght.(map[string]interface{})["Equal"]
-		if Equal(leng.(int), len(respObj.(map[string]interface{}))) == false {
-			return false
+		if !Equal(leng.(int), len(respObj.(map[string]interface{}))) {
+			return newTestCaseResult("Object Lenght not equal", false)
 		}
 	}
 	InType := Contains.(map[string]interface{})["InType"]
@@ -190,41 +193,39 @@ func object(Contains interface{}, respObj interface{}) bool {
 	if InType != nil {
 		return objValue(InType, respObj)
 	}
-	return true
+	return newTestCaseResult("Passed", true)
 }
 
-func objValue(InType interface{}, respObj interface{}) bool {
+func objValue(InType interface{}, respObj interface{}) *TestCaseResult {
 	intype := InType.([]interface{})[0]
 	//fmt.Println(intype)
 
 	for key, value := range intype.(map[string]interface{}) {
 		obj := respObj.(map[string]interface{})[key]
 		if obj == nil {
-			return false
+			return newTestCaseResult("Object item not found", false)
 		}
 		fmt.Println(key, reflect.TypeOf(value), obj)
 
 		val := value.(map[string]interface{})["Contains"]
 		if val != nil {
-			if bodyContains(value, obj) == false {
-				return false
+			res := bodyContains(value, obj)
+			if !res.Result {
+				return res
 			}
 		} else {
-			if objChecks(value, obj) == false {
-				return false
+			if !objChecks(value, obj) {
+				return newTestCaseResult("List item not matched", false)
 			}
 		}
 
 	}
-	return true
+	return newTestCaseResult("Passed", true)
 }
 
 func Equal[T int | string](yml T, resp T) bool {
 	//fmt.Println("equal", yml, resp)
-	if yml == resp {
-		return true
-	}
-	return false
+	return yml == resp
 }
 
 func ResponcetoObject(resp *http.Response) interface{} {
@@ -254,4 +255,11 @@ func ListChecks(ymlval interface{}, resobj interface{}) bool {
 
 	return Equal(ymlval.(string), resobj.(string))
 
+}
+
+func newTestCaseResult(Description string, Result bool) *TestCaseResult {
+
+	tcr := TestCaseResult{Description: Description}
+	tcr.Result = Result
+	return &tcr
 }
